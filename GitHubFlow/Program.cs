0e392d85102b47294d61b271ac675ce4cd85b197
Andrii Pukhkalo <ANDP@simcorp.com>
2025-08-25 14:48:22 +0200

    f3 c1

--- GitHubFlow/Program.cs
+++ GitHubFlow/Program.cs
@@ -5,3 +5,4 @@ Console.WriteLine("fork into                -> feature/f1 -> 0.1.0-alpha.1+1");
 Console.WriteLine("after commit 1 into      -> feature/f1 -> 0.1.0-alpha.1+2");
 
 Console.WriteLine("f2 c1");
+Console.WriteLine("f3 c1");
