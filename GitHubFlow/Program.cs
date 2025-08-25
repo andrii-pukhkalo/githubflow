949c3e337136ffb412db5eb58a482de5c281ca1b
Andrii Pukhkalo <ANDP@simcorp.com>
2025-08-25 14:06:15 +0200

    added f1

--- GitHubFlow/Program.cs
+++ GitHubFlow/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+Console.WriteLine("0.1.0-alpha.1+1 - after branch off -> before commit into branch f1");
